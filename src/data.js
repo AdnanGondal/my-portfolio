e7b3700f0fc04b3f0888273822a4ea58df381bad
Adnan Gondal <adnangondal1@hotmail.com>
2021-11-09 15:53:30 +0000

    initial setup

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    title: "Save a Meal",
+    subtitle: "React, Tailwind, Node, Express, Postgres",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image:
+      "https://user-images.githubusercontent.com/43793942/140721187-6f044142-ac0b-495d-997e-db9469225a0f.png",
+    link: "https://github.com/BledarF/save-a-meal",
+  },
+  {
+    title: "World for Women",
+    subtitle: "React, Bootstrap, Node, Express, Postgres",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image:
+      "https://github.com/gsinghlak12/worldbank/raw/main/User_Manual_images/01-HomePage.png",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = ["JavaScript", "Tailwind", "React", "Node", "Express"];
